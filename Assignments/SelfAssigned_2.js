c90060ce409f1caa9ef46ae524b930c390e269d8
Mohammed Arshad <arshadff02@gmail.com>
2025-01-27 08:05:12 +0530

    added comment for understanding purpose

--- Assignments/SelfAssigned_2.js
+++ Assignments/SelfAssigned_2.js
@@ -1,3 +1,4 @@
+// program to print the numbers from 1 to 10 in reverse order
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 for (let i = 0; i < numbers.length; i++) {
     let number = numbers.pop();
