1e5a1233ea8754c5b5177baba80ef41911ddf1b6
Mohammed Arshad <arshadff02@gmail.com>
2025-01-27 08:04:23 +0530

    new assignment

--- Assignments/SelfAssigned_2.js
+++ Assignments/SelfAssigned_2.js
@@ -1,3 +1,6 @@
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-numbers.reverse();
-console.log(numbers);
+for (let i = 0; i < numbers.length; i++) {
+    let number = numbers.pop();
+    numbers.unshift(number)
+    console.log(number);
+}
